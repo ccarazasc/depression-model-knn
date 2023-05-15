1e9bce53ca36ee13a8ed25e025dd677abf79b3aa
ccarazasc <u20181b541@upc.edu.pe>
2023-05-15 09:27:24 -0500

    ci: deployment config

--- src/main.go
+++ src/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"go-api/src/api"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	var port string = "8080"
+	port := os.Getenv("HTTP_PLATFORM_PORT")
+	//var port string = "8080"
+	if port == "" {
+		port = "8080"
+	}
 
 	router := mux.NewRouter()
 	apiRouter := router.PathPrefix("/api/").Subrouter()
